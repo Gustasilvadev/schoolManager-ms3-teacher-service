198b306c871ddc8350e5da42f7e2d3f41e5f0900
Gustasilvadev <gustamarsil@gmail.com>
2026-04-12 00:54:30 -0300

    feat: implementa teacherService com CRUD e associação de disciplinas

---
+++ src/repositories/teacherDisciplineRepository.js
@@ -0,0 +1,44 @@
+// src/repositories/teacherDisciplineRepository.js
+const prisma = require('../config/prisma');
+
+/**
+ * Associa uma disciplina a um professor
+ */
+const associate = async (teacherId, disciplineId) => {
+  return await prisma.teacher_disciplines.create({
+    data: {
+      teacher_id: teacherId,
+      discipline_id: disciplineId
+    }
+  });
+};
+
+/**
+ * Remove a associação entre professor e disciplina
+ */
+const removeAssociation = async (teacherId, disciplineId) => {
+  return await prisma.teacher_disciplines.deleteMany({
+    where: {
+      teacher_id: teacherId,
+      discipline_id: disciplineId
+    }
+  });
+};
+
+/**
+ * Verifica se já existe uma associação entre professor e disciplina
+ */
+const findByTeacherAndDiscipline = async (teacherId, disciplineId) => {
+  return await prisma.teacher_disciplines.findFirst({
+    where: {
+      teacher_id: teacherId,
+      discipline_id: disciplineId
+    }
+  });
+};
+
+module.exports = {
+  associate,
+  removeAssociation,
+  findByTeacherAndDiscipline
+};
