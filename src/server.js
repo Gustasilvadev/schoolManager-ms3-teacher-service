f8e12fea048a9863789a2068555cc83a5d9f8a02
ogabrielsilvaa <ogabrieusiuva@gmail.com>
2026-05-11 18:50:50 -0300

    feat: atualização para testes via swagger em prod

--- src/server.js
+++ src/server.js
@@ -13,6 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const swaggerDocument = YAML.load('./swagger.yaml');
+if (process.env.SERVER_URL) {
+  swaggerDocument.servers = [{ url: `${process.env.SERVER_URL}/api`, description: 'Servidor atual' }];
+}
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api', routes);
